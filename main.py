eb92990fddbe4c568810ee141d16a2c84622a803
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-03 10:43:32 -0400

    Update and rename main (13).py to main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ conv_fac=0.621371
 miles=kilometers*conv_fac
 print('%0.3f kilometers is equal to %0.3f miles' %(kilometers,miles))
 
-
 celsius=float(input('Enter temperature in Celsius: '))
 fahrenheit=(celsius * 1.8)+32
 print('%0.1f Celsius is equal to %0.1f degree Fahrenheit'%(celsius, fahrenheit))
